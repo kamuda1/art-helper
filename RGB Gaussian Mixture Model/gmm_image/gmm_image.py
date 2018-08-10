1793c76d58695e4ea90e558ad6cec4118b170c7a
Mark Kamuda <kamuda1@illinois.edu>
2018-08-10 13:52:14 -0500

    Corrected import issue

--- RGB Gaussian Mixture Model/gmm_image/gmm_image.py
+++ RGB Gaussian Mixture Model/gmm_image/gmm_image.py
@@ -1,15 +1,14 @@
-import numpy as np
-# from sklearn.mixture import GMM
+from numpy import argmax, argmin
+
 
 def predict_pixel_GMM_cluster(pixel,gmm):
     
     pixel = pixel.reshape(1,3)
-    return np.argmax(gmm.predict_proba(pixel))
+    return argmax(gmm.predict_proba(pixel))
 
 def predict_pixel_GMM_color(pixel,gmm):
     return gmm.means_[predict_pixel_GMM_cluster(pixel,gmm=gmm)]
 
 def predict_pixel_GMM_color_MSE(pixel,gmm):
     pixel_MSE = ((gmm.means_ - pixel) ** 2).mean(axis=1)
-    return gmm.means_[np.argmin(pixel_MSE)]
-
+    return gmm.means_[argmin(pixel_MSE)]
